4939aebcbdeddf536fc1057b80b048072ce5ede8
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-12-17 01:00:40 +0900

    実際の動きに近づける

--- BLH-230K-100/main.ino
+++ BLH-230K-100/main.ino
@@ -4,7 +4,10 @@ void setup() {
 void loop() {
   Motor_SetPose(0);
   delay(1000);
-  Motor_SetPose(180
-  );
-  delay(1000);
+  for(int i;i<5;i++){
+  Motor_SetPose((360/5)*i);
+  delay(3000);
+  Motor_SetPose(0);
+  delay(3000);
+  }
 }
