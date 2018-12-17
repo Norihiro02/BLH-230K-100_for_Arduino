194f15b6bce73bda7473f2170465244f2521208d
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-12-17 13:46:55 +0900

    通信系の作成

--- BLH-230K-100/communication.ino
+++ BLH-230K-100/communication.ino
@@ -1,23 +1,34 @@
-#define RX_DATA_SIZE 2
+#define RX_DATA_SIZE 4
 
 int encode(byte data[])
 {
   int output = 0;
-  for (int i = 0; i <  RX_DATA_SIZE; i++) {
-    output = output << 8;
-    output += data[i];
+
+  for (int i = 1; i <  RX_DATA_SIZE; i++) {
+    /*
+      output = output << 8;
+      output += data[i];
+    */
+    output *= 10;
+    output += data[i] - '0';
+  }
+  if (data[0] == '-') {
+    output *= -1;
+  }
+  else if (data[0] == '+') {
   }
   return output;
 }
+
 int wait_for_data()
 {
   byte rxData[RX_DATA_SIZE];
-  while (2 < Serial.available()) //2バイト受信するまで待機
+  int output = 0;
+  while (RX_DATA_SIZE > Serial.available()); //2バイト受信するまで待機
+  for (int i = 0; i < RX_DATA_SIZE; i++)
   {
-    for (int i; i < RX_DATA_SIZE; i++)
-    {
-      rxData[i]=Serial.read();
-    }
+    rxData[i] = Serial.read();
   }
+
   return encode(rxData);
 }
--- BLH-230K-100/main.ino
+++ BLH-230K-100/main.ino
@@ -1,18 +1,21 @@
 void setup() {
   Motor_init();
-  byte rx_data[]={30,20};
   Serial.begin(9600);
-  Serial.println(encode(rx_data));
+  //Motor_SetPose(0);
+  //Serial.print(encode(rx_data));
 }
 void loop() {
  
-  Motor_SetPose(0);
-  delay(1000);
-  for(int i;i<5;i++){
-  Motor_SetPose((360/5)*i);
-  delay(3000);
-  Motor_SetPose(0);
-  delay(3000);
-  }
+  /*
+  for (int i; i < 5; i++) {
+    Motor_SetPose((360 / 5)*i);
+    delay(3000);
+    Motor_SetPose(0);
+    delay(3000);
+  }*/
   
+  Motor_SetPose(wait_for_data());
+  Motor_Stop();
+  Serial.println(Motor_CurrentPose());
+
 }
